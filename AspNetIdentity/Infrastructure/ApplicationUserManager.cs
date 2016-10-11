9857f46ccffaf091431837bd443d944019bcdf26
Oscar Pérez <mr.omiguelperez@gmail.com>
2016-10-10 23:25:31 -0500

    Enviar correo al crear un usuario

--- AspNetIdentity/Infrastructure/ApplicationUserManager.cs
+++ AspNetIdentity/Infrastructure/ApplicationUserManager.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using AspNetIdentity.Services;
+using AspNetIdentity.Validators;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -19,6 +21,35 @@ namespace AspNetIdentity.Infrastructure
         {
             var appDbContext = context.Get<ApplicationDbContext>();
             var appUserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(appDbContext));
+
+            appUserManager.EmailService = new EmailService();
+
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                appUserManager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"))
+                {
+                    //Code for email confirmation and reset password life time
+                    TokenLifespan = TimeSpan.FromHours(6)
+                };
+            }
+
+            //Configure validation logic for usernames
+            appUserManager.UserValidator = new MyCustomUserValidator(appUserManager)
+            {
+                AllowOnlyAlphanumericUserNames = true,
+                RequireUniqueEmail = true
+            };
+
+            //Configure validation logic for passwords
+            appUserManager.PasswordValidator = new MyCustomPasswordValidator
+            {
+	            RequiredLength = 6,
+	            RequireNonLetterOrDigit = true,
+	            RequireDigit = false,
+	            RequireLowercase = true,
+	            RequireUppercase = true,
+            };
 
             return appUserManager;
         }
---
+++ AspNetIdentity/Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspNetIdentity.Services
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            await configSendGridasync(message);
+        }
+
+        // Use NuGet to install SendGrid (Basic C# client lib) 
+        private async Task configSendGridasync(IdentityMessage message)
+        {
+            using (System.Net.Mail.MailMessage MailSetup = new System.Net.Mail.MailMessage())
+            {
+                NetworkCredential loginInfo = new NetworkCredential("[email]", "[password]");
+                MailSetup.Subject = message.Subject;
+                MailSetup.To.Add(message.Destination);
+                MailSetup.From = new System.Net.Mail.MailAddress("tramites@valledupar.gov.co", "Avanzar es Posible!!!");
+                MailSetup.Body = message.Body;
+                using (System.Net.Mail.SmtpClient SMTP = new System.Net.Mail.SmtpClient("smtp.live.com"))
+                {
+                    SMTP.Port = 587;
+                    SMTP.EnableSsl = true;
+                    SMTP.Credentials = loginInfo;
+                    await SMTP.SendMailAsync(MailSetup);
+                }
+            }
+        }
+    }
+}
