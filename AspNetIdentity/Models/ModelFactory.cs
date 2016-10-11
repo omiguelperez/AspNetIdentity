5b96c5cf5e7c425cc0629044ebfa8ad9a1bdfcbd
Oscar Pérez <mr.omiguelperez@gmail.com>
2016-10-10 22:11:37 -0500

    Initial commit; Primero paso del tutorial

---
+++ AspNetIdentity/Models/ModelFactory.cs
@@ -0,0 +1,39 @@
+﻿using AspNetIdentity.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace AspNetIdentity.Models
+{
+    public class ModelFactory
+    {
+        private UrlHelper _UrlHelper;
+        private ApplicationUserManager _AppUserManager;
+
+        public ModelFactory(HttpRequestMessage request, ApplicationUserManager appUserManager)
+        {
+            _UrlHelper = new UrlHelper(request);
+            _AppUserManager = appUserManager;
+        }
+
+        public UserReturnModel Create(ApplicationUser appUser)
+        {
+            return new UserReturnModel
+            {
+                Url = _UrlHelper.Link("GetUserById", new { id = appUser.Id }),
+                Id = appUser.Id,
+                UserName = appUser.UserName,
+                FullName = string.Format("{0} {1}", appUser.FirstName, appUser.LastName),
+                Email = appUser.Email,
+                EmailConfirmed = appUser.EmailConfirmed,
+                Level = appUser.Level,
+                JoinDate = appUser.JoinDate,
+                Roles = _AppUserManager.GetRolesAsync(appUser.Id).Result,
+                Claims = _AppUserManager.GetClaimsAsync(appUser.Id).Result
+            };
+        }
+    }
+}
